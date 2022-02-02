75c4ed7b6ebadab5633f40319121bd9a999f7f5d
javtushenko <craterec74@yandex.ru>
2022-02-02 12:30:15 +0500

    Init

---
+++ SwiftbookMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftbookMessenger
+//
+//  Created by Николай Явтушенко on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
