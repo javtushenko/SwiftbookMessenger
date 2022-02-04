1c9fce763f09b97ee1da7882adba799fc48e6ddc
javtushenko <craterec74@yandex.ru>
2022-02-04 17:01:54 +0500

    created SetupProfileViewController

--- SwiftbookMessenger/SetupProfileViewController.swift
+++ SwiftbookMessenger/SetupProfileViewController.swift
@@ -11,9 +11,22 @@ class SetupProfileViewController: UIViewController {
     
     let fullImageView = AddPhotoView()
     
+    let welcomeLabel = UILabel(text: "Расскажите о себе!", font: .avenir26())
+    let fullNameLabel = UILabel(text: "Полное имя:")
+    let aboutMeLabel = UILabel(text: "Обо мне:")
+    let sexLabel = UILabel(text: "Ваш пол:")
+    
+    let fullNameTextField = OneLineTextField(font: .avenir20())
+    let aboutMeTextField = OneLineTextField(font: .avenir20())
+    
+    let sexSegmentedControl = UISegmentedControl(first: "Мужской", second: "Женский")
+    
+    let goToChatsButton = UIButton(titleColor: .white, title: "В чат", backgroundcolor: .buttonBlack())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
         setupConstraints()
     }
 }
@@ -23,12 +36,42 @@ class SetupProfileViewController: UIViewController {
 extension SetupProfileViewController {
     
     private func setupConstraints() {
+        
+        let fillNameStackView = UIStackView(arrangedSubviews: [fullNameLabel,
+                                                               fullNameTextField], axis: .vertical, spacing: 0)
+        let aboutMeStackView = UIStackView(arrangedSubviews: [aboutMeLabel,
+                                                              aboutMeTextField], axis: .vertical, spacing: 0)
+        let sexStackView = UIStackView(arrangedSubviews: [sexLabel,
+                                                          sexSegmentedControl], axis: .vertical, spacing: 12)
+        
+        goToChatsButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        let stackView = UIStackView(arrangedSubviews: [fillNameStackView,
+                                                       aboutMeStackView,
+                                                       sexStackView,
+                                                       goToChatsButton], axis: .vertical, spacing: 40)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
         fullImageView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(welcomeLabel)
         view.addSubview(fullImageView)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            fullImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
+            welcomeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            fullImageView.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 60),
             fullImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: fullImageView.bottomAnchor, constant: 60),
+            stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -40)
         ])
     }
 }
