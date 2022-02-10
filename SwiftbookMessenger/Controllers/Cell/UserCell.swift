c26c684d572b5646400cb4c2596661195e3b5503
javtushenko <craterec74@yandex.ru>
2022-02-10 19:42:30 +0500

    Update UserCell

--- SwiftbookMessenger/Controllers/Cell/UserCell.swift
+++ SwiftbookMessenger/Controllers/Cell/UserCell.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class UserCell: UICollectionViewCell, SellConfiguringCell {
     
+    let userImageView = UIImageView()
+    let userName = UILabel(text: "text", font: .laoSangamMN20())
+    let containerView = UIView()
+    
     static var reuseId = "UserCell"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .gray
+        backgroundColor = .white
+        setupConstraints()
+        
+        self.layer.shadowColor = UIColor(rgb: 0xBDBDBD).cgColor
+        self.layer.shadowRadius = 3
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.containerView.layer.cornerRadius = 4
+        self.containerView.clipsToBounds = true
     }
     
     required init?(coder: NSCoder) {
@@ -21,7 +37,50 @@ class UserCell: UICollectionViewCell, SellConfiguringCell {
     }
     
     func configure<U>(with value: U) where U : Hashable {
-        print("what?")
+        guard let user = value as? MUser else { return }
+        userImageView.image = UIImage(named: user.avatarStringURL)
+        userName.text = user.username
+    }
+    
+}
+
+
+//MARK: - Setup constraints
+extension UserCell {
+    
+    private func setupConstraints() {
+        
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        userName.translatesAutoresizingMaskIntoConstraints = false
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        userImageView.backgroundColor = .red
+        
+        addSubview(containerView)
+        containerView.addSubview(userImageView)
+        containerView.addSubview(userName)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: self.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            userImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            userImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            userImageView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            userImageView.heightAnchor.constraint(equalTo: containerView.widthAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            userName.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
+            userName.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8),
+            userName.topAnchor.constraint(equalTo: userImageView.bottomAnchor),
+            userName.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+        
     }
     
 }
